7acb38dcc2eddded763ec177b1ab518f937cece9
malkomich <malkomich@gmail.com>
2016-03-15 19:16:59 +0100

    Scripts of initialization and rollback for the search bar feature

--- src/test/resources/database/search_company_by_name-rollback.sql
+++ src/test/resources/database/search_company_by_name-rollback.sql
@@ -4,3 +4,13 @@
  * Database rollback for the feature of searching a company by name
  */
 
+DELETE FROM role;
+DELETE FROM branch;
+DELETE FROM company;
+DELETE FROM category;
+
+DELETE FROM address;
+DELETE FROM city;
+DELETE FROM country;
+
+DELETE FROM person;
