82a3d8f156873a1cac4bc33c9c14a64c5313011e
Juan Carlos González Cabrero <malkomich@gmail.com>
2016-03-19 18:44:08 +0100

    Optimize SQL initialization script to let prove all features

--- project_documentation/database/initialization.sql
+++ project_documentation/database/initialization.sql
@@ -5,7 +5,8 @@
  * Database initialization with the basic data to check the finished user stories
  */
 
-INSERT INTO person(first_name, last_name, email, password, active, admin_role) VALUES('Nombre', 'Apellidos', 'user@domain.com', 'pass', true, false);
+INSERT INTO person(first_name, last_name, email, password, active, admin_role) VALUES('Usuario 1', 'Apellidos ', 'user1@domain.x', 'pass', true, false);
+INSERT INTO person(first_name, last_name, email, password, active, admin_role) VALUES('Usuario 2', 'Apellidos 2', 'user2@domain.x', 'pass', true, false);
 INSERT INTO person(first_name, last_name, email, password, active, admin_role) VALUES('Juan Carlos', 'González', 'malkomich@gmail.com', 'pass', true, false);
 INSERT INTO person(first_name, last_name, email, password, active, admin_role) VALUES('Admin', 'Admin', 'admin@busy.com', 'pass', true, true);
 
@@ -15,16 +16,16 @@ INSERT INTO address(address1, address2, zip_code, city_id) VALUES('Calle Mayor',
 INSERT INTO address(address1, address2, zip_code, city_id) VALUES('Calle Toreros', null, '47007', (SELECT id FROM city WHERE name='Valladolid'));
 
 INSERT INTO category(name) VALUES('Mobiliario');
-INSERT INTO company(trade_name, business_name, email, cif, active, category_id) VALUES('Boom', 'Boom S.A.', 'jefe@boom.com', 'B12345678', true, (SELECT id FROM category WHERE name='Mobiliario'));
+INSERT INTO company(trade_name, business_name, email, cif, active, category_id) VALUES('Boom', 'Boom S.A.', 'jefe@boom.x', 'B12345678', true, (SELECT id FROM category WHERE name='Mobiliario'));
 INSERT INTO branch(company_id, address_id, main, phone) VALUES((SELECT id FROM company WHERE cif='B12345678'), (SELECT id FROM address WHERE address1='Calle Mayor'), true, '902202122');
-INSERT INTO role(person_id, branch_id, is_manager, activity) VALUES((SELECT id FROM person WHERE email='user@domain.com'), (SELECT id FROM branch WHERE phone='902202122'), true, 'Jefe');
+INSERT INTO role(person_id, branch_id, is_manager, activity) VALUES((SELECT id FROM person WHERE email='user@domain.x'), (SELECT id FROM branch WHERE phone='902202122'), true, 'Jefe');
 
 INSERT INTO category(name) VALUES('Software');
 INSERT INTO company(trade_name, business_name, email, cif, active, category_id) VALUES('Busy', 'Busy S.A.', 'busy.validation@gmail.com', 'B12345679', false, (SELECT id FROM category WHERE name='Software'));
 INSERT INTO branch(company_id, address_id, main, phone) VALUES((SELECT id FROM company WHERE cif='B12345679'), (SELECT id FROM address WHERE address1='Calle Mayor'), true, '902202123');
-INSERT INTO role(person_id, branch_id, is_manager, activity) VALUES((SELECT id FROM person WHERE email='malkomich@gmail.com'), (SELECT id FROM branch WHERE phone='902202122'), true, 'Jefe')
+INSERT INTO role(person_id, branch_id, is_manager, activity) VALUES((SELECT id FROM person WHERE email='malkomich@gmail.com'), (SELECT id FROM branch WHERE phone='902202122'), true, 'Jefe');
 
-INSERT INTO notification(person_id, notif_type, message, read, create_date) VALUES((SELECT id FROM person WHERE email='user@domain.com'), 'Gestión de empresa', 'Su empresa está pendiente de aprobación', true, DEFAULT);
-INSERT INTO notification(person_id, notif_type, message, read, create_date) VALUES((SELECT id FROM person WHERE email='user@domain.com'), 'Gestión de empresa', 'Su empresa ha sido aprobada con éxito', DEFAULT, DEFAULT);
+INSERT INTO notification(person_id, notif_type, message, read, create_date) VALUES((SELECT id FROM person WHERE email='user@domain.x'), 'Gestión de empresa', 'Su empresa está pendiente de aprobación', true, DEFAULT);
+INSERT INTO notification(person_id, notif_type, message, read, create_date) VALUES((SELECT id FROM person WHERE email='user@domain.x'), 'Gestión de empresa', 'Su empresa ha sido aprobada con éxito', DEFAULT, DEFAULT);
 
 INSERT INTO notification(person_id, notif_type, message, read, create_date) VALUES((SELECT id FROM person WHERE email='malkomich@gmail.com'), 'Gestión de empresa', 'Su empresa está pendiente de aprobación', DEFAULT, DEFAULT);
