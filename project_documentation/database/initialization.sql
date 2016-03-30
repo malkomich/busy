0a6b2141143039d99d34941159b7c78c4fcb38bf
Juan Carlos González Cabrero <malkomich@gmail.com>
2016-03-30 21:11:36 +0200

    Fix initialization script

--- project_documentation/database/initialization.sql
+++ project_documentation/database/initialization.sql
@@ -20,7 +20,7 @@ INSERT INTO address(address1, address2, zip_code, city_id) VALUES('Calle Toreros
 INSERT INTO category(name) VALUES('Mobiliario');
 INSERT INTO company(trade_name, business_name, email, cif, active, category_id) VALUES('Boom', 'Boom S.A.', 'jefe@boom.x', 'B12345678', true, (SELECT id FROM category WHERE name='Mobiliario'));
 INSERT INTO branch(company_id, address_id, main, phone) VALUES((SELECT id FROM company WHERE cif='B12345678'), (SELECT id FROM address WHERE address1='Calle Mayor'), true, '902202122');
-INSERT INTO role(person_id, branch_id, is_manager, activity) VALUES((SELECT id FROM person WHERE email='user@domain.x'), (SELECT id FROM branch WHERE phone='902202122'), true, 'Jefe');
+INSERT INTO role(person_id, branch_id, is_manager, activity) VALUES((SELECT id FROM person WHERE email='user1@domain.x'), (SELECT id FROM branch WHERE phone='902202122'), true, 'Jefe');
 
 INSERT INTO category(name) VALUES('Software');
 INSERT INTO company(trade_name, business_name, email, cif, active, category_id) VALUES('Busy', 'Busy S.A.', 'busy.validation@gmail.com', 'B12345679', false, (SELECT id FROM category WHERE name='Software'));
@@ -41,6 +41,6 @@ INSERT INTO hour_schedule(day_schedule_id, start_time, end_time) VALUES((SELECT 
 INSERT INTO hour_schedule(day_schedule_id, start_time, end_time) VALUES((SELECT id FROM day_schedule WHERE day_of_week=2), '12:00:00', '13:30:00');
 INSERT INTO hour_schedule(day_schedule_id, start_time, end_time) VALUES((SELECT id FROM day_schedule WHERE day_of_week=2), '15:00:00', '16:00:00');
 
-INSERT INTO booking((SELECT id FROM person WHERE email='user1@domain.com'), (SELECT id FROM hour_schedule WHERE start_time='23:00:00'))
-INSERT INTO booking((SELECT id FROM person WHERE email='user1@domain.com'), (SELECT id FROM hour_schedule WHERE start_time='12:00:00'))
-INSERT INTO booking((SELECT id FROM person WHERE email='user2@domain.com'), (SELECT id FROM hour_schedule WHERE start_time='12:00:00'))
+INSERT INTO booking(person_id, hour_schedule_id) VALUES((SELECT id FROM person WHERE email='user1@domain.x'), (SELECT id FROM hour_schedule WHERE start_time='23:00:00'));
+INSERT INTO booking(person_id, hour_schedule_id) VALUES((SELECT id FROM person WHERE email='user1@domain.x'), (SELECT id FROM hour_schedule WHERE start_time='12:00:00'));
+INSERT INTO booking(person_id, hour_schedule_id) VALUES((SELECT id FROM person WHERE email='user2@domain.x'), (SELECT id FROM hour_schedule WHERE start_time='12:00:00'));
