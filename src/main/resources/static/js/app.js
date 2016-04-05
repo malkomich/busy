55817c33bc867078983d27055318c5fecf1e9846
Juan Carlos González Cabrero <malkomich@gmail.com>
2016-04-05 20:06:04 +0200

    JS Format refactor

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -6,30 +6,28 @@ $(function() {
     var date = new Date();
     var month = date.getMonth() + 1;
 
-    var options =
-            {
-                events_source : BOOKINGS_PATH + "?branchId=" + branchId + "&year=" + date.getFullYear() + "&month="
-                        + month,
-                view : 'month',
-                tmpl_path : '/tmpls/',
-                tmpl_cache : false,
-                day : date.toString('yyyy-MM-dd'),
-                onAfterViewLoad : function(view) {
-                    $('.page-header h3').text(this.getTitle());
-                    $('.btn-group button').removeClass('active');
-                    $('button[data-calendar-view="' + view + '"]').addClass('active');
-                },
-                classes : {
-                    months : {
-                        general : 'label'
-                    }
-                },
-                language : 'es-ES',
-                modal : "#events-modal",
-                modal_title : function(event) {
-                    return event.title
-                }
-            };
+    var options = {
+        events_source : BOOKINGS_PATH + "?branchId=" + branchId + "&year=" + date.getFullYear() + "&month=" + month,
+        view : 'month',
+        tmpl_path : '/tmpls/',
+        tmpl_cache : false,
+        day : date.toString('yyyy-MM-dd'),
+        onAfterViewLoad : function(view) {
+            $('.page-header h3').text(this.getTitle());
+            $('.btn-group button').removeClass('active');
+            $('button[data-calendar-view="' + view + '"]').addClass('active');
+        },
+        classes : {
+            months : {
+                general : 'label'
+            }
+        },
+        language : 'es-ES',
+        modal : "#events-modal",
+        modal_title : function(event) {
+            return event.title
+        }
+    };
 
     var calendar = $('#calendar').calendar(options);
 
