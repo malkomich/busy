2b94e0df857512cc9bd708c6850e436d760d0fa9
Juan Carlos González Cabrero <malkomich@gmail.com>
2016-05-10 15:47:52 +0200

    Update test scripts

--- src/test/resources/database/manage_service_types-rollback.sql
+++ src/test/resources/database/manage_service_types-rollback.sql
@@ -9,11 +9,6 @@ DELETE FROM booking;
 DELETE FROM service;
 DELETE FROM service_type;
 
-DELETE FROM hour_schedule;
-DELETE FROM day_schedule;
-DELETE FROM week_schedule;
-DELETE FROM year_schedule;
-
 DELETE FROM role;
 DELETE FROM branch;
 DELETE FROM company;
