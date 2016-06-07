59fb384f0fcaad5e2366b54a5cae464e1b0dcff3
Juan Carlos González Cabrero <malkomich@gmail.com>
2016-06-07 21:10:18 +0200

    Add click listener for service creation to day number span too

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -58,20 +58,33 @@ function setupListeners() {
 
     // Day cells
     $('.cal-cell').dblclick(function() {
-        calendar.options.day = $('[data-cal-date]', this).data('cal-date');
+        var date = $('[data-cal-date]', this).data('cal-date');
+        openServiceForm(date);
+    });
+    $('*[data-cal-date]').click(function() {
+        var date = $(this).data('cal-date');
+        openServiceForm(date);
+    });
+}
 
-        $.get("/service_form", {
-            date : calendar.options.day
-        }, function(data) {
-            
-            if($(data).is("p")) {
-                messageModal($(data).text());
-                
-            } else if ($(data).is("form")) {
-                var modalContainer = $('#modalForm');
-                $('.modal-content', modalContainer).html(data);
-                modalContainer.modal();
-            }
-        });
+/*
+ * Open the service modal form for a given day.
+ */
+function openServiceForm(date) {
+
+    calendar.options.day = date;
+
+    $.get("/service_form", {
+        date : calendar.options.day
+    }, function(data) {
+
+        if ($(data).is("p")) {
+            messageModal($(data).text());
+
+        } else if ($(data).is("form")) {
+            var modalContainer = $('#modalForm');
+            $('.modal-content', modalContainer).html(data);
+            modalContainer.modal();
+        }
     });
 }
